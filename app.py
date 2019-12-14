59b6ea164efce0fbe7548522235414849827ddae
sb-raza <syedrazatx@gmail.com>
2019-12-14 11:50:06 -0600

    Updated county selection code to allow for going back

--- app.py
+++ app.py
@@ -1,46 +1,82 @@
 from utilities import format_result
 from counties import fbcad, hcad
 import pyperclip
+import os
 import re
 
-selection = input('(1) FBCAD or (2) HCAD > ')
 
-if selection == '1':
-    # Ask the user for the address to search
-    query = input("\nEnter FBCAD Property Address > ")
+def start_program():
+    """
+    Clears the command prompt screen
+    Then initiates the program
+    """
 
-    # Running a search on the address
-    result = fbcad.get_property_id(query.strip())
+    # Clears the cmd screen
+    os.system('cls')
+    selection = input('(1) FBCAD or (2) HCAD > ')
+    go_to(selection)
 
-    # Checking if the property was found
-    if result:
-        pyperclip.copy(format_result(result))
+
+def go_to(selection):
+    """
+    Processes the User selection to route the script
+    Enables functionality to go back to start_program()
+    """
+
+    if selection == '1':
+        # Ask the user for the address to search
+        print('\nFort Bend County selected. Enter (x) to go back.\n')
+        query = input("Enter Property Address > ")
+
+        # If user inputs x, restart the program
+        if query.lower() == 'x':
+            start_program()
+
+        else:
+            # Running a search on the address
+            result = fbcad.get_property_id(query.strip())
+
+            # Checking if the property was found
+            if result:
+                pyperclip.copy(format_result(result))
+
+            else:
+                print("Property Not Found")
+                pyperclip.copy("Property Not Found")
+
+    elif selection == '2':
+        # Ask the user for the address to search
+        print('\nHarris County selected. Enter (x) to go back.\n')
+        query = input("Enter Property Address > ")
+
+        # If user inputs x, restart the program
+        if query.lower() == 'x':
+            start_program()
+
+        else:
+            # Split the inputted address into street number and street name using regex
+            # Find all the numbers at the start of the address
+            street_number = re.findall(r'^[0-9]*', query)[0]
+            # Find all the characters in the address that are not numbers
+            street_name = re.findall(r'[^0-9]+', query)[0].strip()
+
+            # Running a search on the address
+            try:
+                result = hcad.get_data(stnum=street_number, stname=street_name)
+
+            except AttributeError:
+                result = None
+
+            # Checking if the property was found
+            if result:
+
+                pyperclip.copy(format_result(result))
+            else:
+                print("Property Not Found")
+                pyperclip.copy("Property Not Found")
+
     else:
-        print("Property Not Found")
-        pyperclip.copy("Property Not Found")
+        start_program()
 
-elif selection == '2':
-    # Ask the user for the address to search
-    query = input("\nEnter HCAD Property Address > ")
 
-    # Split the inputted address into street number and street name using regex
-    # Find all the numbers at the start of the address
-    street_number = re.findall(r'^[0-9]*', query)[0]
-    # Find all the characters in the address that are not numbers
-    street_name = re.findall(r'[^0-9]+', query)[0].strip()
-
-    # Running a search on the address
-    try:
-        result = hcad.get_data(stnum=street_number, stname=street_name)
-    except AttributeError:
-        result = None
-
-    # Checking if the property was found
-    if result:
-        pyperclip.copy(format_result(result))
-    else:
-        print("Property Not Found")
-        pyperclip.copy("Property Not Found")
-
-else:
-    print('Invalid input')
+start_program()
